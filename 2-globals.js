dc2714bd74532fe19d7a25f73b5f953c3ee26e45
DylanParenzee <dylanparenzee@hotmail.com>
2022-09-10 15:52:33 +1000

    globals section completed

---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+// Globals
+
+// __dirname - path to current directiory
+// __filename = file name
+
+// requre - function to use modules
+// module - info about current module
+// process - info about env where the program is being excecuted
+
+console.log(__dirname);
+
+// setInterval(() => {
+//   console.log("hello World");
+// }, 1000);
