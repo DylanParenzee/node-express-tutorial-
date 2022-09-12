bd5707a5072b0148bdda0b5a1993aab060596d74
DylanParenzee <dylanparenzee@hotmail.com>
2022-09-13 06:51:39 +1000

    typo

--- 2-globals.js
+++ 2-globals.js
@@ -3,7 +3,7 @@
 // __dirname - path to current directiory
 // __filename = file name
 
-// requre - function to use modules
+// require - function to use modules
 // module - info about current module
 // process - info about env where the program is being excecuted
 
